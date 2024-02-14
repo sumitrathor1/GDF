d5a137d8cb33d677d7ca2db69370d192da1b37a6
sumitrathor1 <144085591+sumitrathor1@users.noreply.github.com>
2024-02-14 19:25:33 +0530

    Add files via upload

---
+++ Volunteer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gurukul Dream Foundation - Volunteer</title>
+    <meta name="description" content="Welcome to Gurukul Dream Foundation, where we empower the youth of tier 2/3 cities. Our goal is to provide tailored support and opportunities for young individuals to thrive. Through workshops, internships, and more, we aim to equip them with the skills they need to succeed. Join us as we shape a brighter future for every aspiring young mind.">
+    <meta name="keywords" content="Gurukul Dream Foundation, where we empower the youth of tier 2/3 cities. Our goal is to provide tailored support and opportunities for young individuals to thrive. Through workshops, internships, and more, we aim to equip them with the skills they need to succeed. Join us as we shape a brighter future for every aspiring young mind.">
+    <link rel="icon" href="./favicon.ico" type="image/x-icon">
+    <link href="./assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body class="overflow-x-hidden">
+    <?php include "_header.php"?>
+
+    <?php include "_footer.php"?>
+    <script src="./assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ _header.php
@@ -0,0 +1,43 @@
+<header>
+    <nav class="navbar navbar-expand-lg bg-black border-bottom border-body overflow-x-hidden" data-bs-theme="dark">
+        <div class="container-fluid container responsive-navbar">
+            <div class="header-logo col-auto   align-self-center mx-3 ">
+                <a class="navbar-brand  mx-5" href="./">
+                    <img src="./assets/images/logo.png" alt="Logo">
+                </a>
+            </div>
+            <button class="navbar-toggler align-self-start m-2 position-absolute top-0" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="col-auto align-self-start  px-2 px-md-0">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item hover-effect navbar-links-box">
+                            <a class="nav-link text-white hover-effect  navbar-links d-flex flex-column" aria-current="page" href="./">Home</a>
+                        </li>
+                        <li class="nav-item hover-effect navbar-links-box">
+                            <a class="nav-link text-white hover-effect navbar-links d-flex flex-column" href="What-We-Do.php">What We Do</a>
+                        </li>
+                        <li class="nav-item hover-effect navbar-links-box">
+                            <a class="nav-link text-white hover-effect navbar-links d-flex flex-column" href="Our-Achievements.php">Our Achievements</a>
+                        </li>
+                        <li class="nav-item hover-effect navbar-links-box">
+                            <a class="nav-link text-white hover-effect navbar-links d-flex flex-column" href="The-Team.php">The Team</a>
+                        </li>
+                        <li class="nav-item hover-effect navbar-links-box">
+                            <a class="nav-link text-white hover-effect navbar-links d-flex flex-column" href="Volunteer.php">Volunteer</a>
+                        </li>
+                        <li class="nav-item hover-effect navbar-links-box">
+                            <a class="nav-link text-white hover-effect navbar-links d-flex flex-column" href="Contacts.php">Contacts</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <a href="./Donate.php" class="btn btn-warning">
+                Donate
+            </a>
+        </div>
+    </nav>
+</header>
